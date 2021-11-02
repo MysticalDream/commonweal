1550694bbb55c00b9f9e98a152771aa1297349c4
MysticalDream <1817764079@qq.com>
2021-11-02 22:08:23 +0800

    .

---
+++ src/main/java/team/skylinekids/commonweal/dao/test/TestDao.java
@@ -0,0 +1,10 @@
+package team.skylinekids.commonweal.dao.test;
+
+import team.skylinekids.commonweal.dao.core.BaseDao;
+
+/**
+ * @author MysticalDream
+ */
+public class TestDao extends BaseDao {
+
+}
---
+++ src/main/java/team/skylinekids/commonweal/dao/testpo/Province.java
@@ -0,0 +1,34 @@
+package team.skylinekids.commonweal.dao.testpo;
+
+/**
+ * @author MysticalDream
+ */
+public class Province {
+    private String province;
+    private String city;
+    private String are;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAre() {
+        return are;
+    }
+
+    public void setAre(String are) {
+        this.are = are;
+    }
+}
