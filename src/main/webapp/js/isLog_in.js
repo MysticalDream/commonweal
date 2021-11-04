7ea07c7b554f03523627ecaf334ee95a620faa7d
sjm <2674114136@qq.com>
2021-11-04 20:11:30 +0800

    修改了登录注册

--- src/main/webapp/js/isLog_in.js
+++ src/main/webapp/js/isLog_in.js
@@ -1,20 +1,36 @@
 window.addEventListener('load', () => {
     // 判断有没有userid 有的话页面的登录和注册要改变 没有的话就不做任何操作
+    let slideshow = document.querySelector('.slideshow');
     if (getCookie("userId") !== "undefined") {
-        // 获取登录和注册两个圈圈
-        let log_in = document.querySelector('.log_in');
-        let sign_in = document.querySelector('.sign_in');
-        // 修改注册按钮
-        // sign_in.parentNode.removeChild(sign_in);
+        // 创建登录和注册两个圈圈
+        // let log_in = document.querySelector('.log_in');
+        // let sign_in = document.querySelector('.sign_in');
+        // 注册按钮
+        let log_in = document.createElement('div');
+        log_in.classList.add('log_in');
+        log_in.style.background = "rgba(255,255,255,0)"
+        let sign_in = document.createElement('div');
+        sign_in.classList.add('sign_in');
         sign_in.classList.add('sign_in2');
         sign_in.innerHTML = getCookie("username");
+        // sign_in.parentNode.removeChild(sign_in);
         // 将登录按钮的内容换成头像
-        log_in.innerHTML = '';
-        log_in.style.background = "rgba(255,255,255,0)"
         let img = document.createElement('img');
         img.src = getCookie("avatarUrl");
         log_in.appendChild(img);
-        log_in.classList.add("have_login");
-        sign_in.classList.add("have_login");
+        // log_in.classList.add("have_login");
+        // sign_in.classList.add("have_login");
+        slideshow.appendChild(log_in);
+        slideshow.appendChild(sign_in);
+    } else {
+        let log_in = document.createElement('div');
+        log_in.classList.add('log_in');
+        log_in.style.background = '#fff';
+        log_in.innerHTML = '登录';
+        let sign_in = document.createElement('div');
+        sign_in.classList.add('sign_in');
+        sign_in.innerHTML = '注册';
+        slideshow.appendChild(log_in);
+        slideshow.appendChild(sign_in);
     }
 })
--- src/main/webapp/js/isLog_in_adpot.js
+++ src/main/webapp/js/isLog_in_adpot.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
     // 用户已登录
+
     if (getCookie("userid") != undefined) {
-        // 获取username的盒子 为其添加两个子盒子
         let username = document.querySelector('.username');
         let pic = document.createElement('div');
         pic.classList.add('pic');
