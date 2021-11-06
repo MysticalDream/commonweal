df3a0d41e783ebf258f6db41a306893300f6313f
sjm <2674114136@qq.com>
2021-11-06 19:46:50 +0800

    加个×号

--- src/main/webapp/js/join_in.js
+++ src/main/webapp/js/join_in.js
@@ -2,8 +2,9 @@ window.addEventListener('load', () => {
     let first = document.querySelector('.mask_join>.first');
     let second = document.querySelector('.mask_join>.second');
     let wait = document.querySelector('.wait');
-    let mask_join=document.querySelector('.mask_join');
-    let mask_join_in=document.querySelector('.mask_join_in');
+    let mask_join = document.querySelector('.mask_join');
+    let mask_join_in = document.querySelector('.mask_join_in');
+    let icon_fork = document.querySelector('.icon-fork');
     // 获取项目id
     first.addEventListener('click', () => {
             wait.style.display = 'block';
@@ -16,13 +17,15 @@ window.addEventListener('load', () => {
     second.addEventListener('click', () => {
         // 给个标记 然后跳转
         // window.location.href = 'join_in_box.html';
-        mask_join.style.display='none';
-        setTimeout(()=>{
-            mask_join_in.style.display='block';
-            mask_join_in.classList.add('drop_down');
-        },200)
-        // 要获取团队人数
-        // targetid!=2
+        mask_join.style.display = 'none';
+        setTimeout(() => {
+                mask_join_in.style.display = 'block';
+                mask_join_in.classList.add('drop_down');
+            }, 200)
+            // 要获取团队人数
+            // targetid!=2
     })
-
+    icon_fork.addEventListener('click', () => {
+        mask_join.style.display = 'none';
+    })
 })
