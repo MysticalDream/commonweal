084d76a8bd4b25091c7c909cd7bf3c981abe606e
sjm <2674114136@qq.com>
2021-11-22 23:34:49 +0800

    云墙

--- src/main/webapp/js/cloudWall.js
+++ src/main/webapp/js/cloudWall.js
@@ -8,12 +8,42 @@ window.addEventListener('load', () => {
     const rcards = document.querySelector('.right .cards');
     const lbox = document.querySelector('.left .box');
     const lcards = document.querySelector('.left .cards');
-    rwrite.addEventListener('click', () => {
-        window.location.href = 'cloudWall2.html';
-    })
+    let pageNum = 1;
+    let pageSize = 4;
+    let flag = false;
+    var data = {
+            "1": ['11111', '2222222', '333333333', '10-'],
+            "2": ['4444444', '55555555', '66666666', '11-'],
+            "3": ['777777', '88888888', '999999999', '12-']
+        }
+        /**
+         * 节流函数 减少执行次数
+         * @param {*} func 业务代码
+         * @param {*} delay 延时
+         */
+    function throttle(func, delay) {    
+        var flag = true;    
+        return function(e) {      
+            if (flag) {        
+                setTimeout(() => {          
+                    func.call(this, e);          
+                    flag = true;        
+                }, delay);      
+            }      
+            flag = false;    
+        };  
+    }
+
     lwrite.addEventListener('click', () => {
+        flag = false;
         window.location.href = 'cloudWall2.html';
     })
+    rwrite.addEventListener('click', () => {
+            flag = true;
+            window.location.href = 'cloudWall2.html';
+        })
+        // 要判断是不是第一页 是第一页的话就不触发
+
     window.addEventListener('mousewheel', (e) => {
         let eve = e || window.event;
         // 阻止默认行为
@@ -48,33 +78,43 @@ window.addEventListener('load', () => {
             }
         }
     }, { passive: false });
-    rbox.addEventListener('mousewheel', (e) => {
+    rbox.addEventListener('mousewheel', throttle(function(e) {
         let evt = e || window.event;
         evt.stopPropagation();
         if (evt.deltaY < 0) { //鼠标滚轮向上滚动
-            console.log(rcards.offsetLeft + 5);
-            rcards.style.left = rcards.offsetLeft - 100 + 'px';
+            console.log(rcards.offsetLeft + 100);
+            rcards.style.left = rcards.offsetLeft - 1080 + 'px';
+            pageNum++;
         } else { //鼠标滚轮向下滚动
             console.log(2);
             if (rcards.offsetLeft >= 0) {
                 rcards.style.left = '0px';
             } else {
-                rcards.style.left = rcards.offsetLeft + 100 + 'px';
+                rcards.style.left = rcards.offsetLeft + 1080 + 'px';
             }
+            pageNum--;
         }
-    })
-    lbox.addEventListener('mousewheel', (e) => {
+    }, 3000))
+    for (let i in data) {
+        for (let j = 0; j < data[i].length; j++) {
+            rcards.innerHTML += `
+                <li>${data[i][j]}</li>
+            `;
+        }
+        // pageNum++;
+    }
+    lbox.addEventListener('mousewheel', throttle(function(e) {
         let evt = e || window.event;
         evt.stopPropagation();
         if (evt.deltaY < 0) {
-            lcards.style.left = lcards.offsetLeft - 100 + 'px';
+            lcards.style.left = lcards.offsetLeft - 1080 + 'px';
         } else {
             console.log(lcards.offsetLeft);
             if (lcards.offsetLeft >= 0) {
                 lcards.style.left = '0px'
             } else {
-                lcards.style.left = lcards.offsetLeft + 100 + 'px';
+                lcards.style.left = lcards.offsetLeft + 1080 + 'px';
             }
         }
-    })
+    }))
 })
